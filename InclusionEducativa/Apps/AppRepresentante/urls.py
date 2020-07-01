6d8b01ef84dd8d91ee56096807ab5185b7675d36
jafetandres <46006892+jafetandres@users.noreply.github.com>
2020-07-01 17:41:58 -0500

    Update urls.py

--- InclusionEducativa/Apps/AppRepresentante/urls.py
+++ InclusionEducativa/Apps/AppRepresentante/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
+from django.urls import path
+
 from InclusionEducativa.Apps.AppRepresentante.views import *
 
 app_name = 'apprepresentante'
 urlpatterns = [
     url(r'^$', base, name='base'),
     url(r'^buscarEstudiante$', buscarEstudiante, name='buscarEstudiante'),
-    url(r'^perfil$', perfil, name='perfil'),
+    path('perfil/', perfil, name='perfil'),
     url(r'^crearFichaInformativa/(?P<estudiante_cedula>\d+)/$', crearFichaInformativa, name='crearFichaInformativa'),
     url(r'^verFichaInformativa/(?P<cedula>\d+)/$', verFichaInformativa, name='verFichaInformativa'),
 
